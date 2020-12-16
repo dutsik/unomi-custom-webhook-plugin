c5d65fafd5a3a7aa538f9bb57af303fd67ab7680
Peter876 <piatro97@gmail.com>
2020-12-16 12:48:05 +0300

    Fix source code

--- src/main/java/org/peter/unomi/customwebhook/actions/SendRequestAction.java
+++ src/main/java/org/peter/unomi/customwebhook/actions/SendRequestAction.java
@@ -1,29 +1,34 @@
 package org.peter.unomi.customwebhook.actions;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ConnectTimeoutException;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.apache.unomi.api.Event;
 import org.apache.unomi.api.Session;
 import org.apache.unomi.api.actions.Action;
 import org.apache.unomi.api.actions.ActionExecutor;
-import org.slf4j.Logger;import org.apache.unomi.api.services.EventService;
+import org.apache.unomi.api.services.EventService;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class SendRequestAction implements ActionExecutor {
     private String customWebhookUrlBase;
     private String unomiElasticSearchIndexPreafix;
+    private String webhookName;
+    private String url;
     private static final String text = "1234567890";
     private static Logger logger = LoggerFactory.getLogger(SendRequestAction.class);
     private CloseableHttpClient httpClient;
@@ -31,27 +36,45 @@ public class SendRequestAction implements ActionExecutor {
     @Override
     public int execute(Action action, Event event) {
         if (httpClient == null) {
-            httpClient = HttpClients.createDefault();
+            int timeout = 5;
+            RequestConfig requestConfig = RequestConfig.custom()
+                    .setConnectTimeout(timeout * 1000)
+                    .setConnectionRequestTimeout(timeout * 1000)
+                    .setSocketTimeout(timeout * 1000).build();
+            httpClient = HttpClientBuilder.create().setDefaultRequestConfig(requestConfig).build();
             }
         Session session = event.getSession();
         if (customWebhookUrlBase == null) {
-            logger.warn("Configuration incomplete.");
+            logger.warn("customWebhookUrlBase is empty.");
             return EventService.NO_CHANGE;
+        }
+        if (webhookName == null) {
+            webhookName = "default";
+            logger.warn("webhookName is empty. Using default webhook");
+        }
+        if (!customWebhookUrlBase.endsWith("/")) {
+            customWebhookUrlBase += "/";
         }
         String accountId = (String) event.getProperty("accountId");
         String eventId = event.getItemId();
         String hash = "35454B055CC325EA1AF2126E27707052";
-        String param1 = eventId + text;
         String md5Hex = DigestUtils
-          .md5Hex(param1).toUpperCase();
-        final HttpPost httpPost = new HttpPost(customWebhookUrlBase);
+          .md5Hex(eventId).toUpperCase();
+        String param1 = eventId + text;
+        url = customWebhookUrlBase + webhookName;
+        final HttpPost httpPost = new HttpPost(url);
         final List<NameValuePair> params = new ArrayList<>();
-        params.add(new BasicNameValuePair("accountId", md5Hex));
-        params.add(new BasicNameValuePair("eventId", eventId));
+        if (accountId != null) {
+            params.add(new BasicNameValuePair("accountId", accountId));
+        }
+        else {
+            logger.warn("accountId is empty");
+        }
+        params.add(new BasicNameValuePair("eventId", param1));
         try {
             httpPost.setEntity(new UrlEncodedFormEntity(params));
         } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+            logger.error("Unsupported encoding.", e);
         }
         CloseableHttpResponse response = null;
         try {
@@ -59,10 +82,16 @@ public class SendRequestAction implements ActionExecutor {
             int statusCode = response.getStatusLine().getStatusCode();
             if (statusCode != 200) {
                 logger.error("Error with the API response.");
+                return EventService.NO_CHANGE;
             }
-        } catch (IOException e) {
+        }
+        catch(ConnectTimeoutException e) {
+        	logger.error("Error with the Http Request execution. Response timed out.", e);
+        }
+        catch (IOException e) {
             logger.error("Error with the Http Request execution. Wrong parameters given", e);
-        } finally {
+        }
+        finally {
             if (response != null) {
                 EntityUtils.consumeQuietly(response.getEntity());
             }
