41129ef60bbb6cf4e74e2f25ab23bc2983c09389
Ivan Adutskevich <dutsik@gmail.com>
2020-12-21 13:16:00 +0300

    Update the way we define property to send
    - implement new logic
    - update action definition
    - rewire config properties

--- src/main/java/org/peter/unomi/customwebhook/actions/SendRequestAction.java
+++ src/main/java/org/peter/unomi/customwebhook/actions/SendRequestAction.java
@@ -2,6 +2,7 @@ package org.peter.unomi.customwebhook.actions;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +18,6 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.apache.unomi.api.Event;
-import org.apache.unomi.api.Session;
 import org.apache.unomi.api.actions.Action;
 import org.apache.unomi.api.actions.ActionExecutor;
 import org.apache.unomi.api.services.EventService;
@@ -25,50 +25,70 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SendRequestAction implements ActionExecutor {
-    private String customWebhookUrlBase;
-    private String unomiElasticSearchIndexPreafix;
-    private String webhookName;
-    private String url;
-    private static final String text = "1234567890";
-    private static Logger logger = LoggerFactory.getLogger(SendRequestAction.class);
+    private String customWebhookUrlBase = null;
+    private static final Logger logger = LoggerFactory.getLogger(SendRequestAction.class);
     private CloseableHttpClient httpClient;
+    private String unomiElasticSearchIndexPrefix = null;
 
     @Override
     public int execute(Action action, Event event) {
+
+        logger.info("Start webhook action.");
         if (httpClient == null) {
-            int timeout = 5;
+            int timeout = 1;
             RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(timeout * 1000)
                     .setConnectionRequestTimeout(timeout * 1000).setSocketTimeout(timeout * 1000).build();
             httpClient = HttpClientBuilder.create().setDefaultRequestConfig(requestConfig).build();
         }
-        Session session = event.getSession();
-        if (customWebhookUrlBase == null) {
-            logger.warn("customWebhookUrlBase is empty.");
+
+        if (this.customWebhookUrlBase == null) {
+            logger.error("customWebhookUrlBase is empty.");
             return EventService.NO_CHANGE;
         }
-        webhookName = action.getParameterValues().get("webhookName").toString();
-        if (webhookName == null) {
+        Object webhookNameProperty = action.getParameterValues().get("webhookName");
+        String webhookName;
+
+        if (webhookNameProperty == null) {
             webhookName = "default";
             logger.warn("webhookName is empty. Using default webhook");
-        }
-        if (!customWebhookUrlBase.endsWith("/")) {
-            customWebhookUrlBase += "/";
-        }
-        String propertyName = action.getParameterValues().get("propertyName").toString();
-        String propertyValue = event.getProperty(propertyName).toString();
-        String eventId = event.getItemId();
-        String hash = "35454B055CC325EA1AF2126E27707052";
-        String md5Hex = DigestUtils.md5Hex(eventId).toUpperCase();
-        String param1 = md5Hex + text;
-        url = customWebhookUrlBase + webhookName;
-        final HttpPost httpPost = new HttpPost(url);
-        final List<NameValuePair> params = new ArrayList<>();
-        if (propertyValue != null) {
-            params.add(new BasicNameValuePair(propertyName, propertyValue));
         } else {
-            logger.warn("accountId is empty");
+            webhookName = webhookNameProperty.toString();
         }
-        params.add(new BasicNameValuePair("eventId", param1));
+
+        if (!this.customWebhookUrlBase.endsWith("/")) {
+            this.customWebhookUrlBase += "/";
+        }
+
+        Object propertyName = action.getParameterValues().get("propertyName");
+        Object propertyValue = action.getParameterValues().get("propertyValue");
+
+        String eventId = event.getItemId();
+
+        String hash = "35454B055CC325EA1AF2126E27707052";
+        String md5Hex = DigestUtils.md5Hex(eventId + hash).toUpperCase();
+
+        String url = this.customWebhookUrlBase + webhookName;
+
+        final HttpPost httpPost = new HttpPost(url);
+
+        final List<NameValuePair> params = new ArrayList<>();
+        if (propertyValue != null && propertyName != null) {
+            params.add(new BasicNameValuePair(propertyName.toString(), propertyValue.toString()));
+        } else {
+            logger.warn("Custom Property is empty");
+        }
+
+        if (this.unomiElasticSearchIndexPrefix != null) {
+            params.add(new BasicNameValuePair("prefix", this.unomiElasticSearchIndexPrefix));
+        }
+
+        params.add(new BasicNameValuePair("eventId", eventId));
+        params.add(new BasicNameValuePair("checkSum", md5Hex));
+        if (logger.isDebugEnabled()) {
+            logger.debug("webhook Url {}", url);
+            logger.debug("Property Name {}", action.getParameterValues().get("propertyName"));
+            logger.debug("Property Value {}", action.getParameterValues().get("propertyValue"));
+        }
         try {
             httpPost.setEntity(new UrlEncodedFormEntity(params));
         } catch (UnsupportedEncodingException e) {
@@ -80,20 +100,27 @@ public class SendRequestAction implements ActionExecutor {
             response = httpClient.execute(httpPost);
             int statusCode = response.getStatusLine().getStatusCode();
             if (statusCode != 200) {
-                logger.error("Error with the API response.");
+                logger.error("Error with the API response with Status Code = {}", statusCode);
                 return EventService.NO_CHANGE;
             }
-        } catch (ConnectTimeoutException e) {
+        } catch (ConnectTimeoutException | SocketTimeoutException e) {
             logger.error("Error with the Http Request execution. Response timed out.", e);
             return EventService.NO_CHANGE;
         } catch (IOException e) {
-            logger.error("Error with the Http Request execution. Wrong parameters given", e);
+            logger.error("Error with the Http Request execution. ", e);
             return EventService.NO_CHANGE;
         } finally {
             if (response != null) {
                 EntityUtils.consumeQuietly(response.getEntity());
             }
         }
-        return EventService.SESSION_UPDATED;
+        return EventService.NO_CHANGE;
     }
+    public void setCustomWebhookUrlBase(String customWebhookUrlBase) {
+        this.customWebhookUrlBase = customWebhookUrlBase;
+    }
+    public void setUnomiElasticSearchIndexPrefix(String unomiElasticSearchIndexPrefix) {
+        this.unomiElasticSearchIndexPrefix = unomiElasticSearchIndexPrefix;
+    }
+
 }
